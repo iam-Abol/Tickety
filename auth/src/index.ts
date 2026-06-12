b0dfc86f8d5742b589aa60688a7676aa5f39a00f
unknown <mh.z77w@gmail.com>
2026-06-12 22:03:41 +0330

    add ingress-nginx

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,10 @@ const app = express();
 
 app.use(express.json());
 
+app.get("/api/users/currentuser", (req, res) => {
+  res.send("HIiiiii testing ");
+});
+
 app.listen(3000, () => {
   console.log("listening on port 3000!");
 });
