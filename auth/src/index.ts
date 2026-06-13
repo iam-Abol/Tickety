ead3f56c5fc0eeacc46b9ba7a5e612b87990ba98
unknown <mh.z77w@gmail.com>
2026-06-13 14:53:48 +0330

    feat(auth): create seperate route handeler for current user

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,12 +1,11 @@
 import express from "express";
+import { currentUserRouter } from "./routes/current-user";
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/api/users/currentuser", (req, res) => {
-  res.send("HIiiiii testing ");
-});
+app.use(currentUserRouter);
 
 app.listen(3000, () => {
   console.log("listening on port 3000!");
