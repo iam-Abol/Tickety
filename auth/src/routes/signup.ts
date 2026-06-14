a2476d36e8fdbc3b0b2833a9be60cb4065926a2e
unknown <mh.z77w@gmail.com>
2026-06-14 14:13:41 +0330

    feat(auth.signup): handle validation errors

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response } from "express";
-import { body } from "express-validator";
+import { body, validationResult } from "express-validator";
 const router = express.Router();
 
 router.post(
@@ -12,7 +12,16 @@ router.post(
       .withMessage("Password must be between 4 and 20 character"),
   ],
   (req: Request, res: Response) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      // return res.send(errors.array());
+      throw new Error("Invalid email or password");
+    }
+
     const { email, password } = req.body;
+    console.log("Creating a user...");
+    res.send({});
   }
 );
 
