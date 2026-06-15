2e492225fa5247efdc6bf38bc4356e1bbddffd68
unknown <mh.z77w@gmail.com>
2026-06-15 16:32:25 +0330

    feat(errorHandler middleware): convert errors to responses with currect formats

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,4 +1,6 @@
 import { NextFunction, Request, Response } from "express";
+import { RequestValidationError } from "../errors/request-validation-errror";
+import { DatabaseConnectionError } from "../errors/database-connection-error";
 
 export const errorHandler = (
   err: Error,
@@ -6,7 +8,18 @@ export const errorHandler = (
   res: Response,
   next: NextFunction
 ) => {
+  if (err instanceof RequestValidationError) {
+    const formattedErrors = err.errors.map((error) => {
+      if (error.type === "field") {
+        return { message: error.msg, field: error.path };
+      }
+    });
+    return res.status(400).send({ errors: formattedErrors });
+  }
+  if (err instanceof DatabaseConnectionError) {
+    return res.status(500).send({ errors: [{ message: err.reason }] });
+  }
   console.log("Something went wrong", err);
 
-  res.status(400).send({ message: err.message });
+  res.status(400).send({ errors: [{ message: "something went wrong" }] });
 };
