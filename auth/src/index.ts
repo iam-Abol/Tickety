fb52b7d9cebee3cb7509db930150d46f8937077c
unknown <mh.z77w@gmail.com>
2026-06-15 19:36:16 +0330

    feat(index): handle async errors with a npm package

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,4 +1,6 @@
 import express from "express";
+import "express-async-errors";
+
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
 import { signoutRouter } from "./routes/signout";
@@ -15,7 +17,7 @@ app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
 
-app.all("*", () => {
+app.all("*", async (req, res, next) => {
   throw new NoteFoundError();
 });
 
