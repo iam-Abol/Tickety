d529e46f01ebc5499e4653348fd94c23ddf1f489
unknown <mh.z77w@gmail.com>
2026-06-15 19:04:36 +0330

    feat(errors): implement abstract CustomError class for serializing errors

--- auth/src/errors/database-connection-error.ts
+++ auth/src/errors/database-connection-error.ts
@@ -1,10 +1,15 @@
-export class DatabaseConnectionError extends Error {
-    reason = "Error connecting to database";
-  
-    constructor() {
-      super();
-  
-      Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
-    }
+import { CustomError } from "./custom-error";
+
+export class DatabaseConnectionError extends CustomError {
+  statusCode = 500;
+  reason = "Error connecting to database";
+
+  constructor() {
+    super("Error connecting to db ");
+
+    Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
   }
-  
+  serializeErrors() {
+    return [{ message: this.reason }];
+  }
+}
--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,6 +1,5 @@
 import { NextFunction, Request, Response } from "express";
-import { RequestValidationError } from "../errors/request-validation-errror";
-import { DatabaseConnectionError } from "../errors/database-connection-error";
+import { CustomError } from "../errors/custom-error";
 
 export const errorHandler = (
   err: Error,
@@ -8,18 +7,9 @@ export const errorHandler = (
   res: Response,
   next: NextFunction
 ) => {
-  if (err instanceof RequestValidationError) {
-    const formattedErrors = err.errors.map((error) => {
-      if (error.type === "field") {
-        return { message: error.msg, field: error.path };
-      }
-    });
-    return res.status(400).send({ errors: formattedErrors });
+  if (err instanceof CustomError) {
+    return res.status(err.statusCode).send({ errors: err.serializeErrors() });
   }
-  if (err instanceof DatabaseConnectionError) {
-    return res.status(500).send({ errors: [{ message: err.reason }] });
-  }
-  console.log("Something went wrong", err);
 
   res.status(400).send({ errors: [{ message: "something went wrong" }] });
 };
