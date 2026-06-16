d9c56ed2f62f8d87ae61c05c905491e98db7289a
unknown <mh.z77w@gmail.com>
2026-06-16 20:06:07 +0330

    feat(auth): connect to mongodb and create the user schema

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import "express-async-errors";
-
+import mongoose from "mongoose";
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
 import { signoutRouter } from "./routes/signout";
@@ -23,6 +23,16 @@ app.all("*", async (req, res, next) => {
 
 app.use(errorHandler);
 
-app.listen(3000, () => {
-  console.log("listening on port 3000!");
-});
+const start = async () => {
+  try {
+    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
+  } catch (error) {
+    console.error(error);
+  }
+
+  app.listen(3000, () => {
+    console.log("listening on port 3000!");
+  });
+};
+
+start();
