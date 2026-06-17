00a7ea3c296a655d95197744447a0a301538ab4f
unknown <mh.z77w@gmail.com>
2026-06-17 13:00:56 +0330

    feat(auth.services.password): implement compare method

--- auth/src/services/password.ts
+++ auth/src/services/password.ts
@@ -7,5 +7,10 @@ export class Password {
     const buf = (await scryptAsync(password, salt, 64)) as Buffer;
     return `${buf.toString("hex")}.${salt}`;
   }
-  static compare(storedPassword: string, suppliedPassword: string) {}
+  static async compare(storedPassword: string, suppliedPassword: string) {
+    const [hashedPassword, salt] = storedPassword.split('.');
+    const buf = (await scryptAsync(suppliedPassword, salt, 64)) as Buffer;
+
+    return buf.toString('hex') === hashedPassword;
+  }
 }
