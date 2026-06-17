d7e94e2cff4ad86d35cf4d5f4b1c723aceb84c54
unknown <mh.z77w@gmail.com>
2026-06-17 12:40:22 +0330

    feat(auth): implement signup method and set password class up for hashing and comparing passwords

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -2,6 +2,8 @@ import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
 import { RequestValidationError } from "../errors/request-validation-errror";
 import { DatabaseConnectionError } from "../errors/database-connection-error";
+import { User } from "../models/user";
+import { BadRequestError } from "../errors/bad-request-error";
 const router = express.Router();
 
 router.post(
@@ -13,7 +15,7 @@ router.post(
       .isLength({ max: 20, min: 4 })
       .withMessage("Password must be between 4 and 20 character"),
   ],
-  (req: Request, res: Response) => {
+  async (req: Request, res: Response) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
@@ -21,9 +23,17 @@ router.post(
     }
 
     const { email, password } = req.body;
-    console.log("Creating a user...");
-    throw new DatabaseConnectionError();
-    res.send({});
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      throw new BadRequestError("Email in use");
+    }
+
+    const user = User.build({ email, password });
+    await user.save();
+
+    res.status(201).send(user);
   }
 );
 
---
+++ auth/src/services/password.ts
@@ -0,0 +1,4 @@
+export class Password {
+  static toHash(password: string) {}
+  static compare(storedPassword: string, suppliedPassword: string) {}
+}
