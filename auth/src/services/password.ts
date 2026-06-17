4fa44adb73974d1c05246d9f52b6f2d7b6e8533c
unknown <mh.z77w@gmail.com>
2026-06-17 12:56:34 +0330

    feat(auth.services.password): implement toHash method

--- auth/src/services/password.ts
+++ auth/src/services/password.ts
@@ -1,4 +1,11 @@
+import { scrypt, randomBytes } from "crypto";
+import { promisify } from "util";
+const scryptAsync = promisify(scrypt);
 export class Password {
-  static toHash(password: string) {}
+  static async toHash(password: string) {
+    const salt = randomBytes(8).toString("hex");
+    const buf = (await scryptAsync(password, salt, 64)) as Buffer;
+    return `${buf.toString("hex")}.${salt}`;
+  }
   static compare(storedPassword: string, suppliedPassword: string) {}
 }
