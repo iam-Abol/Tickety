fb2c0b0ffef53553572ad0c935b4c1729eafa23c
unknown <mh.z77w@gmail.com>
2026-06-20 22:17:27 +0330

    feat(auth): setup cookie-session

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -7,11 +7,11 @@ import { signoutRouter } from "./routes/signout";
 import { signupRouter } from "./routes/signup";
 import { errorHandler } from "./middlewares/error-handler";
 import { NoteFoundError } from "./errors/not-found-error";
-
+import cookieSession from "cookie-session";
 const app = express();
-
+app.set("trust proxy", true);
 app.use(express.json());
-
+app.use(cookieSession({ signed: false, secure: true }));
 app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
