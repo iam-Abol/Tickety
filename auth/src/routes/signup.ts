a28efd1629ce4f2c4a798a891cc28f531413d194
unknown <mh.z77w@gmail.com>
2026-06-21 22:15:46 +0330

    feat(auth): impliment validateRequest middleware to avoid code duplication

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,11 +1,11 @@
 import express, { Request, Response } from "express";
-import { body, validationResult } from "express-validator";
-import { RequestValidationError } from "../errors/request-validation-errror";
-import { DatabaseConnectionError } from "../errors/database-connection-error";
+import { body } from "express-validator";
+
 import { User } from "../models/user";
 import { BadRequestError } from "../errors/bad-request-error";
 const router = express.Router();
 import jwt from "jsonwebtoken";
+import { validateRequest } from "../middlewares/validate-request";
 router.post(
   "/api/users/signup",
   [
@@ -15,13 +15,8 @@ router.post(
       .isLength({ max: 20, min: 4 })
       .withMessage("Password must be between 4 and 20 character"),
   ],
+  validateRequest,
   async (req: Request, res: Response) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      throw new RequestValidationError(errors.array());
-    }
-
     const { email, password } = req.body;
 
     const existingUser = await User.findOne({ email });
@@ -44,5 +39,5 @@ router.post(
     res.status(201).send(user);
   }
 );
- 
+
 export { router as signupRouter };
