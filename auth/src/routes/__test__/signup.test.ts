52b087c510acd8c71457191cd4db70d71341b4a5
unknown <mh.z77w@gmail.com>
2026-06-23 13:46:22 +0330

    test(auth.signup) : add tests for invalid body

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -10,3 +10,24 @@ it("returns a 201 on successful signup", () => {
     })
     .expect(201);
 });
+it("returns a 400 with an invalid email", () => {
+  return request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test",
+      password: "123456",
+    })
+    .expect(400);
+});
+it("returns a 400 with an invalid password", () => {
+  return request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@gmail.com",
+      password: "1",
+    })
+    .expect(400);
+});
+it("returns a 400 with missing email and password", () => {
+  return request(app).post("/api/users/signup").send({}).expect(400);
+});
