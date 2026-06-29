c8e91076f29aa9910020302bc782c3365fb6d7be
unknown <mh.z77w@gmail.com>
2026-06-29 13:41:21 +0330

    feat(client): add header compoenent and signout functionality

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,4 +1,5 @@
 import buildClient from "../api/build-client";
+
 const LandingPage = ({ currentUser }) => {
   return currentUser ? (
     <h1>You are signed in</h1>
@@ -8,7 +9,10 @@ const LandingPage = ({ currentUser }) => {
 };
 
 LandingPage.getInitialProps = async (context) => {
-  const { data } = await buildClient(context).get("/api/users/currentuser");
+  console.log("LANDING PAGE!");
+  const client = buildClient(context);
+  const { data } = await client.get("/api/users/currentuser");
+
   return data;
 };
 
