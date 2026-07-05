02d5b961c5120feed2b47a0845be491d26584994
unknown <mh.z77w@gmail.com>
2026-07-05 14:11:53 +0330

    feat(infra): add mongo uri as an enviroment variable to deployments

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -5,8 +5,12 @@ const start = async () => {
   if (!process.env.JWT_KEY) {
     throw new Error("JWT_KEY must be defind");
   }
+  if (!process.env.MONGO_URI) {
+    throw new Error("MONGO_URI must be defind");
+  }
   try {
-    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("connectd to tickets mongo db");
   } catch (error) {
     console.error(error);
   }
