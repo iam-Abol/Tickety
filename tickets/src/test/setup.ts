1a4deac13750c3e25511be9f254ef05864a7061f
unknown <mh.z77w@gmail.com>
2026-07-07 15:22:07 +0330

    feat(ticket): implememt update route

--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -2,6 +2,7 @@ import { MongoMemoryServer } from "mongodb-memory-server";
 import { app } from "../app";
 import mongoose from "mongoose";
 import request from "supertest";
+import jwt from "jsonwebtoken";
 let mongo: any;
 beforeAll(async () => {
   mongo = await MongoMemoryServer.create();
@@ -30,17 +31,19 @@ declare global {
   var signin: () => Promise<string[]>;
 }
 
-global.signin = async () => {
-  const email = "test@gmail.com";
-  const password = "123456";
-  const res = await request(app)
-    .post("/api/users/signup")
-    .send({ email, password })
-    .expect(201);
-  const cookie = res.get("Set-Cookie");
+global.signin = () => {
+  const payload = {
+    id: new mongoose.Types.ObjectId().toHexString(),
+    email: "test@test.com",
+  };
 
-  if (!cookie) {
-    throw new Error("Failed to get cookie from response");
-  }
-  return cookie;
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
+
+  const session = { jwt: token };
+
+  const sessionJSON = JSON.stringify(session);
+
+  const base64 = Buffer.from(sessionJSON).toString("base64");
+
+  return [`session=${base64}`];
 };
