3ccca42dcb7b62d5a57880f9976c02d9ebf24c3d
unknown <mh.z77w@gmail.com>
2026-07-21 15:06:01 +0330

    chore: setup orders service

---
+++ orders/src/app.ts
@@ -0,0 +1,32 @@
+import express from "express";
+import "express-async-errors";
+import { errorHandler, NoteFoundError, currentUser } from "@zayatickety/common";
+import cookieSession from "cookie-session";
+import { indexOrderRouter } from "./routes";
+import { showOrderRouter } from "./routes/show";
+import { newOrderRouter } from "./routes/new";
+import { deleteOrderRouter } from "./routes/delete";
+
+const app = express();
+app.set("trust proxy", true);
+app.use(express.json());
+app.use(
+  cookieSession({ signed: false, secure: process.env.NODE_ENV !== "test" })
+);
+
+app.use(currentUser);
+
+app.use(indexOrderRouter);
+app.use(showOrderRouter);
+app.use(newOrderRouter);
+app.use(deleteOrderRouter);
+
+////////////////
+
+app.all("*", async (req, res, next) => {
+  throw new NoteFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
