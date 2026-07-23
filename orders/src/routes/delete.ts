e2b4d81db5a5cd11a3fcce32c5db69694fbaaf2e
unknown <mh.z77w@gmail.com>
2026-07-23 12:23:14 +0330

    feat(orders): implement delete route

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,9 +1,33 @@
-import express, { Request, Response } from 'express';
+import {
+  NotAuthorizedError,
+  NoteFoundError,
+  OrderStatus,
+  requireAuth,
+} from "@zayatickety/common";
+import express, { Request, Response } from "express";
+import { Order } from "../models/order";
 
 const router = express.Router();
 
-router.delete('/api/orders/:orderId', async (req: Request, res: Response) => {
-  res.send({});
-});
+router.delete(
+  "/api/orders/:orderId",
+  requireAuth,
+  async (req: Request, res: Response) => {
+    const { orderId } = req.params;
+
+    const order = await Order.findById(orderId);
+
+    if (!order) {
+      throw new NoteFoundError();
+    }
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+    order.status = OrderStatus.Cancelled;
+    await order.save();
+
+    res.status(204).send(order);
+  },
+);
 
 export { router as deleteOrderRouter };
